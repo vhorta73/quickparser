fcd2892caa747406ad8db80247977c8d0a66cd7d
Vasco Horta <diasfonseca@live.com>
2023-12-08 19:09:42 +0000

    Build

---
+++ lib/Parser2023.pm
@@ -0,0 +1,27 @@
+package Parser2023;
+
+=head1 NAME
+
+Parser2023 - Quick parser.
+
+=head1 SYNOPSIS
+
+  perl Build.PL
+
+=head1 DESCRIPTION
+
+=cut
+
+BEGIN {
+  use Exporter;
+  use vars     qw{ $VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS };
+  $VERSION     = '1.0.0';
+  push @ISA,   qw{ Exporter };
+ 
+  @EXPORT      = qw{};
+  @EXPORT_OK   = qw{};
+  %EXPORT_TAGS = ();
+}
+
+1;
+__END__
